c42a44ea2f86bf42ab4a0c9034f75a6827daf69e
Nelson Elhage <nelhage@nelhage.com>
2018-12-25 18:07:31 +0000

    13.2

--- 13/simulate.py
+++ 13/simulate.py
@@ -1,5 +1,6 @@
 import attr
 import sys
+import time
 import numpy as np
 
 @attr.s(slots=True)
@@ -25,13 +26,97 @@ for x in range(cols):
     else:
       cell = text[y][x]
     if cell == '<' or cell == '>':
+      carts.append(Cart(x, y, cell))
       cell = '-'
-      carts.append(Cart(x, y, cell))
     elif cell == '^' or cell == 'v':
-      cell = '|'
       carts.append(Cart(x, y, cell))
+      cell = '|'
     grid[x,y] = ord(cell)
 for cart in carts:
   cartmap[cart.x, cart.y] = 1
 
-print("\n".join(''.join(map(chr, row)) for row in np.transpose(grid)))
+# print("\n".join(''.join(map(chr, row)) for row in np.transpose(grid)))
+
+MOVES = {
+  '<': (-1, 0),
+  '>': ( 1, 0),
+  '^': ( 0, -1),
+  'v': ( 0, 1),
+}
+# (left, straight, right)
+TURNS = {
+  '<': 'v<^',
+  '>': '^>v',
+  '^': '<^>',
+  'v': '>v<',
+}
+CORNERS = {
+  ('>', ord('\\')): 'v',
+  ('>', ord('/')):  '^',
+  ('<', ord('/')):  'v',
+  ('<', ord('\\')): '^',
+
+  ('v', ord('\\')): '>',
+  ('v', ord('/')):  '<',
+  ('^', ord('/')):  '>',
+  ('^', ord('\\')): '<',
+}
+
+PRINT = False
+
+tick = 0
+remove = set()
+while True:
+  if PRINT:
+    # print("before tick={}".format(tick))
+    render = np.copy(grid)
+    for cart in carts:
+      render[cart.x, cart.y] = ord(cart.dir)
+    txt = "\n".join(''.join(map(chr, row)) for row in np.transpose(render))
+    sys.stdout.write("\x1b[H\x1b[J")
+    print(txt)
+    # time.sleep(0.5)
+
+  tick += 1
+  carts.sort(key=lambda c: (c.y, c.x))
+
+  tmp = np.zeros(grid.shape, dtype=np.uint8)
+  for cart in carts:
+    tmp[cart.x, cart.y] = 1
+  if not np.all(tmp == cartmap):
+    ix, iy = np.unravel_index(np.argmax(tmp != cartmap, axis=None), tmp.shape)
+    print("invariant violated at={},{}".format(ix, iy))
+    sys.exit(1)
+
+  for cart in carts:
+    if id(cart) in remove:
+      continue
+    cartmap[cart.x, cart.y] = 0
+    v = MOVES[cart.dir]
+    cart.x += v[0]
+    cart.y += v[1]
+    if cartmap[cart.x, cart.y]:
+      remove.update(id(o) for o in carts if o.x == cart.x and o.y == cart.y)
+      if len(remove) != 2:
+        print("??? remove={}".format(len(remove)))
+        print("carts={}".format([o for o in carts if o.x == cart.x and o.y == cart.y]))
+      cartmap[cart.x, cart.y] = 0
+      continue
+    sq = grid[cart.x, cart.y]
+    if sq == ord('+'):
+      cart.dir = TURNS[cart.dir][cart.isection % 3]
+      cart.isection += 1
+    else:
+      cart.dir = CORNERS.get((cart.dir, sq), cart.dir)
+    cartmap[cart.x, cart.y] = 1
+
+  if remove:
+    carts = [c for c in carts if id(c) not in remove]
+    remove.clear()
+    if len(carts) == 1:
+      c = carts[0]
+      print("done tick={} pos={},{}".format(tick, c.x, c.y))
+      break
+    elif len(carts) == 0:
+      print("oops, no carts left tick={}".format(tick))
+      break
